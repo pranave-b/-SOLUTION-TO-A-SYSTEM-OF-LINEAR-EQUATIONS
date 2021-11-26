6b51739e9389a7917b00b8eabba7e9879310f764
pranave-b <pranave862003@gmail.com>
2021-11-26 11:01:06 +0530

    sys_lin_eq

--- # -SOLUTION-TO-A-SYSTEM-OF-LINEAR-EQUATI.py
+++ # -SOLUTION-TO-A-SYSTEM-OF-LINEAR-EQUATI.py
@@ -15,13 +15,22 @@ Using the np.linalg.solve(), we can find the solutions.
 End the program
 ## Program:
 #Program to find the solution for the given linear equations.
+
 #Developed by: PRANAVE B
+
 #RegisterNumber:21500582
+
+
 import numpy as np
+
 A=np.array([[1,-3],[3,1]])
+
 B=np.array([0,10])
+
 sol=np.linalg.solve(A,B)
+
 print(sol)
+
 ## Output:
 ![github logo](sys_eq.png)
 ## Result: 
